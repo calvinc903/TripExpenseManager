1e7cdee3ca7657bf27ec3b8f2703ac62d2be2f24
Calvin Cheah <calvin10424@gmail.com>
2024-07-30 04:11:12 +0800

    Initial Commit

---
+++ TripPurchaseManager/TripPurchaseManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPurchaseManagerApp.swift
+//  TripPurchaseManager
+//
+//  Created by Calvin Cheah on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TripPurchaseManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
